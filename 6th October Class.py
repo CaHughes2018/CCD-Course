701ff03642297a8c11bd541c2e05b6148a9db8a4
CaHughes2018 <72097100+CaHughes2018@users.noreply.github.com>
2020-10-06 18:32:58 +0100

    Add files via upload

---
+++ 6th October Class.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name = "Mike"
+print(name)
+
+
+# In[2]:
+
+
+name = "John"
+print (name)
+
+
+# In[3]:
+
+
+#THIS IS MY CLASS FOR TUESDAY OCTOBER 6TH
+
+
+# In[4]:
+
+
+import calendar
+
+c = calendar.TextCalendar(calendar.TUESDAY)
+c.prmonth(2020, 9)
+
+
+# In[5]:
+
+
+import calendar
+
+c = calendar.TextCalendar(calendar.TUESDAY)
+c.prmonth(2020, 10)
+
+
+# In[6]:
+
+
+import calendar
+
+c = calendar.TextCalendar(calendar.TUESDAY)
+c.prmonth(2020, 10)
+
+
+# In[7]:
+
+
+x = float(3)
+
+
+# In[8]:
+
+
+0.2+0.3
+
+
+# In[9]:
+
+
+name = "John 'Cool'Miles"
+print (name)
+
+
+# In[10]:
+
+
+name = True
+print (type (name))
+
+
+# In[ ]:
+
+
+
+
