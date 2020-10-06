07b6214e2f416e22769d93e52334e4c68eac9e3a
CaHughes2018 <72097100+CaHughes2018@users.noreply.github.com>
2020-10-06 19:39:19 +0100

    Add files via upload

---
+++ Shopping list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list = ["bread", "milk", "potatoes", "chicken", "chocolate"]
+print(list)
+
+
+# In[2]:
+
+
+print (list[2])
+
+
+# In[ ]:
+
+
+
+
